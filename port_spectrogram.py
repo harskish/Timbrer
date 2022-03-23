51811b975afc0b390d4f431a59ccd924c0b233cb
Erik Härkönen <erik.harkonen@hotmail.com>
2022-03-23 11:29:45 +0200

    More porting

--- port_spectrogram.py
+++ port_spectrogram.py
@@ -19,18 +19,31 @@ n_mels = 512
 target_shape = (1, 512, 512)
 
 def f_tf(waveform):
-    z = tf.contrib.signal.stft(waveform, frame_length=2*2048, frame_step=500)
+    z = tf.signal.stft(waveform, frame_length=2*2048, frame_step=500, pad_end=True)
     magnitudes = tf.abs(z)
-    filterbank = tf.contrib.signal.linear_to_mel_weight_matrix(
+    filterbank = tf.signal.linear_to_mel_weight_matrix(
         num_mel_bins=n_mels, #80
-        num_spectrogram_bins=magnitudes.shape[-1].value,
+        num_spectrogram_bins=magnitudes.shape[-1],
         sample_rate=sample_rate,
         lower_edge_hertz=0.0,
         upper_edge_hertz=0.5*sample_rate) #8k
     melspectrogram = tf.tensordot(magnitudes, filterbank, 1)
-    return tf.log1p(melspectrogram)[:, :target_shape[1], :target_shape[2]]
+    return tf.math.log1p(melspectrogram)[:, :target_shape[1], :target_shape[2]]
 
-def f_pt(waveform, wftf):
+_converter = None
+def f_pt(waveform):
+    global _converter
+
+    if _converter is None:
+        _converter = torch.nn.Sequential(
+            tl.Spectrogram(n_fft=2*2048, hop_length=500, power=1),
+            tl.LogmelFilterBank(n_fft=2*2048, sr=sample_rate, n_mels=n_mels, fmin=0.0, fmax=0.5*sample_rate, is_log=False)
+        )
+
+    melspectrogram = _converter(waveform)
+    return torch.log1p(melspectrogram)[:, 0, :target_shape[1], :target_shape[2]]
+
+def comp(waveform, wftf, converter=None):
     z1 = tf.signal.stft(wftf, frame_length=2*2048, frame_step=500, pad_end=True)
     #z = tl.Spectrogram(n_fft=2*2048, hop_length=500, power=1)(waveform.unsqueeze(0))
 
@@ -52,27 +65,41 @@ def f_pt(waveform, wftf):
     melspectrogram1 = tf.tensordot(magnitudes1, filterbank, 1)
     #melspectrogram = tl.LogmelFilterBank(n_fft=2*2048, sr=sample_rate, n_mels=n_mels, fmax=0.5*sample_rate, is_log=False)(magnitudes)
     
-    melspectrogram = torch.nn.Sequential(
-        tl.Spectrogram(n_fft=2*2048, hop_length=500, power=1),
-        tl.LogmelFilterBank(n_fft=2*2048, sr=sample_rate, n_mels=n_mels, fmin=0.0, fmax=0.5*sample_rate, is_log=False,
-    ))(waveform.unsqueeze(0))
+    if converter is None:
+        converter = torch.nn.Sequential(
+            tl.Spectrogram(n_fft=2*2048, hop_length=500, power=1),
+            tl.LogmelFilterBank(n_fft=2*2048, sr=sample_rate, n_mels=n_mels, fmin=0.0, fmax=0.5*sample_rate, is_log=False)
+        )
 
-    plt.figure(figsize=(20,20))
+    melspectrogram = converter(waveform.unsqueeze(0))
 
     s1_scaled = (melspectrogram1 / tf.reduce_max(melspectrogram1)).numpy()
     s2_scaled = (melspectrogram / melspectrogram.max())[0, 0, :, :].cpu().numpy()
-
     combined = np.hstack([s1_scaled.T, s2_scaled.T])
+
+    plt.figure(figsize=(15, 15))
     librosa.display.specshow(librosa.amplitude_to_db(combined, ref=np.max), sr=sample_rate, y_axis='mel', x_axis='time')
     plt.show()
 
-    return tf.log1p(melspectrogram)[:, :target_shape[1], :target_shape[2]]
+    return tf.math.log1p(melspectrogram)[:, :target_shape[1], :target_shape[2]]
 
 
-waveform, sr = librosa.load('C:/Users/Erik/code/timbrer/data/wav/shakuhachi.wav', sr=sample_rate, duration=duration)
+waveform, sr = librosa.load('C:/Users/Erik/code/timbrer/data/wav/shakuhachi.wav', sr=sample_rate, duration=2*duration)
 assert sr == sample_rate
+
+# Add batch dim
+waveform = np.stack([waveform[:num_samples], waveform[num_samples:]], axis=0)
 
 wfpt = torch.from_numpy(waveform).to('cpu')
 wftf = tf.constant(waveform)
 
-v = f_pt(wfpt, wftf)
+v1, v2 = f_pt(wfpt).numpy().transpose((0, 2, 1))
+v3, v4 = f_tf(wftf).numpy().transpose((0, 2, 1))
+comb1 = np.hstack([v1 / v1.max(), v3 / v3.max()])
+comb2 = np.hstack([v2 / v2.max(), v4 / v4.max()])
+
+plt.figure(figsize=(15, 25))
+librosa.display.specshow(librosa.amplitude_to_db(comb2, ref=np.max), sr=sample_rate, y_axis='mel', x_axis='time')
+plt.show()
+
+print('Done')
